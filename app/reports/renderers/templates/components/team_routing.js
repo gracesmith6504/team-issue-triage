6aa19c74958cef803685d401754c156908a9448d
Grace Smith <grasmith@redhat.com>
2026-08-11 16:50:07 +0100

    feat: replace 'PR' text with git-merge icon for linked PRs
    - Use GitHub's standard git-merge SVG icon instead of text
    - Green icon (#1A7F37) matches GitHub's styling
    - More visual, easier to scan
    - Matches what Grace saw in dashboard-v3 draft

--- app/reports/renderers/templates/components/team_routing.js
+++ app/reports/renderers/templates/components/team_routing.js
@@ -178,7 +178,7 @@ function buildTeamRouting() {
               '<a class="num" href="' + esc(iss.issue_url) + '">#' + iss.issue_number + '</a>' +
               '<span class="title"><a href="' + esc(iss.issue_url) + '">' + esc(iss.issue_title) + '</a></span>' +
               '<span class="issue-meta">' +
-                (iss.has_linked_pr ? '<span class="pr">PR</span>' : '') +
+                (iss.has_linked_pr ? '<span class="pr-icon"><svg width="16" height="16" viewBox="0 0 16 16" fill="#1A7F37"><path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"/></svg></span>' : '') +
                 (iss.author_login ? '<span>@' + esc(iss.author_login) + '</span>' : '') +
                 authorBadge +
                 (iss.days_open != null ? '<span>' + iss.days_open + 'd</span>' : '') +
